31b526075f78959a52065091104006fbfd2e0c8e
Shanice2021 <80087071+Shanice2021@users.noreply.github.com>
2021-04-16 12:55:44 +0800

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,63 +1,29 @@
 import requests
-import time
-import csv
 from bs4 import BeautifulSoup
-URL="https://www.majortests.com/word-lists/word-list-{:02d}.html"
 
-def generate_urls(url,sp,ep):
-    urls=[]
-    for p in range (sp,ep):
-        urls.append(url.format(p))
-    return urls
 
-def get_resource(url):
+def urlsss(urls):
     headers={
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
     }
-    return requests.get(url, headers=headers)
+    r=requests.get(urls,headers=headers,verify=False)
+    r.encoding = r.apparent_encoding
+    soup = BeautifulSoup(r.text, 'lxml') #lxml = html.parser
+    soup = soup.find('tbody')
+    Ysoup = soup.find_all('p')
+    Ysoup = str(Ysoup)
+    Ysoup=Ysoup.replace('<p>',"")
+    Ysoup=Ysoup.replace('</p>',"")
+    Ysoup=list(Ysoup.split(','))
+    for i in Ysoup:
+        print(i)
 
-def parse_html(html_str):
-    return BeautifulSoup(html_str,"lxml")
-
-def get_word(soup, file):
-    words=[]
-    count = 0
-    for wordlist_table in soup.find_all(class_="wordlist"):
-        count+= 1
-        for word_entry in wordlist_table.find_all("tr"):
-            new_word=[]
-            new_word.append(file)
-            new_word.append(str(count))
-            new_word.append(word_entry.th.text)
-            new_word.append(word_entry.td.text)
-            words.append(new_word)
-    return words
-
-def web_scraping_bot(urls):
-    eng_words=[]
-    for url in urls:
-        file=url.split("/")[-1]
-        print("catching: ",file,"web data...")
-        r=get_resource(url)
-        if r.status_code == requests.codes.ok:
-            soup=parse_html(r.text)
-            words = get_word(soup, file)
-            eng_words =eng_words +words
-            print("waiting 5 seconds.....")
-            time.sleep(5)
-        else:
-            print("HTTP request error!!")
-    return eng_words
-
-def save_to_csv(words, file):
-    with open(file, "w+", newline="", encoding="utf-8")as fp:
-        writer=csv.writer(fp)
-        for word in words:
-            writer.writerow(word)
-
-if __name__=="__main__":
-    urlx=generate_urls(URL, 1, 16)
-    eng_words=web_scraping_bot(urlx)
-    for item in eng_words:
-        print(item)
-    save_to_csv(eng_words,"engWordList_1.csv")
+urlsss('https://csie.asia.edu.tw/project/semester-100')
+urlsss('https://csie.asia.edu.tw/project/semester-101')
+urlsss('https://csie.asia.edu.tw/project/semester-102')
+urlsss('https://csie.asia.edu.tw/project/semester-103')
+urlsss('https://csie.asia.edu.tw/project/semester-104')
+urlsss('https://csie.asia.edu.tw/project/semester-105')
+urlsss('https://csie.asia.edu.tw/project/semester-1061')
+urlsss('https://csie.asia.edu.tw/project/semester-107')
+urlsss('https://csie.asia.edu.tw/project/108學年')
